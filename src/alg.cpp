f335656a4fdbc6dc37d3b5b38ff6beaafe9b7fe9
Смирнова Алёна <99176199+aleonawith@users.noreply.github.com>
2022-02-16 18:44:58 +0300

    Update alg.cpp
    Смирнова Алена 21-ИСТ-2

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,17 +4,48 @@
 
 
 bool checkPrime(uint64_t value) {
-// вставьте код функции
+  if (value == 1) {
+    return false;
+  }
+  for (int i = 2; i * i <= value; i++) {
+    if (value % i == 0) {
+      return false;
+    }
+  }
+  return true;
 }
 
 uint64_t nPrime(uint64_t n) {
-// вставьте код функции
+  int count = 1, value = 0;
+  if (n == 1) {
+    return 2;
+  }
+  while (count <= n) {
+    value++;
+    if (checkPrime(value) == 1) {
+      count++;
+    }
+  }
+  return value;
 }
 
 uint64_t nextPrime(uint64_t value) {
-// вставьте код функции
+  int value1 = value, c = 0;
+  while (c == 0) {
+    value1++;
+    if (checkPrime(value1) == 1) {
+      c++;
+      return value1;
+    }
+  }
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-// вставьте код функции
+  int sum = 0;
+  for (int i = 0; i < hbound; i++) {
+    if (checkPrime(i) == 1) {
+      sum += i;
+    }
+  }
+  return sum;
 }
